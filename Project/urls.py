6151bf49155acaa1b0253aef85f42c26a7896933
Alex <mblahnik@uwm.edu>
2019-03-14 06:40:18 -0500

    Testing  things

--- Project/urls.py
+++ Project/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import url
+from django.urls import path
+from django.contrib import admin
+from Account import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("", views.Home.as_view())
 ]
