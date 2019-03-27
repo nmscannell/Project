f4886d4e24c31df34f415fb6baa58cc860267625
Alex <mblahnik@uwm.edu>
2019-03-27 12:50:20 -0500

    Testing  things

--- main/models.py
+++ main/models.py
@@ -1,18 +1,19 @@
 from django.db import models
 
 
-
 # Create your models here.
 class UI:
     def command(self, inStr):
+
+
         return inStr
 
 
-class createCourse(models.model):
-    name = models.CharField(max_length=30)
-    number = models.CharField(max_length=3)
-    days = models.CharField(max_length=4)
-    start = models.CharField(max_length=4)
-    end = models.CharField(max_length=4)
+#class createCourse(models.model):
+#    name = models.CharField(max_length=30)
+#    number = models.CharField(max_length=3)
+#    days = models.CharField(max_length=4)
+#    start = models.CharField(max_length=4)
+#    end = models.CharField(max_length=4)
 
 
