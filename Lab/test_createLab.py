64d275a43edc3ecf4c9a115d799828c4ec823aac
Erik <spykim2003@gmail.com>
2019-03-29 00:11:17 -0500

    created test_CreateLab file

---
+++ Lab/test_createLab.py
@@ -0,0 +1,13 @@
+from django.test import TestCase
+from Lab.CreateLab import CreateLab
+from Lab.models import models
+
+class test_CreateLab(TestCase):
+
+    def setup(self):
+        pass
+
+    def test_(self):
+        pass
+    def test_(self):
+        pass
