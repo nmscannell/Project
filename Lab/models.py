fe5a7e163c4453b96d4dcbaf7365cab6b77581eb
Erik <spykim2003@gmail.com>
2019-03-29 00:51:30 -0500

    Migrate models to database

--- Lab/models.py
+++ Lab/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class CreateLab(models.Model):
     courseNumber = models.IntegerField(max_length = 5, default= 00000)
     sectionNumber = models.CharField(max_length = 3, default= 000)
-    meetingDays = models.CharField(max_length= 10, default=" ")
+    meetingDays = models.CharField(max_length = 10, default=" ")
     startTime = models.IntegerField(max_length= 10, default="00:00")
     endTime = models.IntegerField(max_length= 10, default="00:00")
 
