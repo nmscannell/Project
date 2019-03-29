469f0a8dfb09a771f397b90e8f5f25492c4574e4
Erik <spykim2003@gmail.com>
2019-03-29 00:35:10 -0500

    created lab -> model.py for createLab

--- Lab/models.py
+++ Lab/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class CreateLab(models.Model):
+    courseNumber = models.IntegerField(max_length = 5, default= 0)
+    sectionNumber = models.CharField(max_length = 3, default= 0)
+    meetingDays = models.CharField(max_length= 10, default=" ")
+    startTime = models.IntegerField(max_length= 10, default="00:00")
+    endTime = models.IntegerField(max_length= 10, default="00:00")
+
+
+
