f8f06449bbc779eae76f8dc65ceb5f8f45bdf311
Erik <spykim2003@gmail.com>
2019-04-02 11:45:27 -0500

    updated test for createLab

--- Lab/Test_CreateLab.py
+++ Lab/Test_CreateLab.py
@@ -3,7 +3,6 @@ from Lab.CreateLab import CreateLab
 from Lab.models import models
 
 
-
 class Test_CreateLab(TestCase):
 
     def setUp(self):
@@ -15,15 +14,15 @@ class Test_CreateLab(TestCase):
                                  meetingDays="M,W", startTime="10:00", endTime="12:00")
 
     def test_lab_was_successfully_created(self):
-        a = CreateLab.objects.get(courseNumber=52312, sectionNumber="001")
+        a = CreateLab.createLab().get(courseNumber=52312, sectionNumber="001")
         self.assertEqual(a.meetingDays, "W")
         self.assertEqual(a.starTime, "10:00")
         self.assertEqual(a.endTime, "12:00")
-        b = CreateLab.objects.get(courseNumber=52312, sectionNumber="002")
+        b = CreateLab.createLab().get(courseNumber=52312, sectionNumber="002")
         self.assertEqual(b.meetingDays, "F")
         self.assertEqual(b.starTime, "14:00")
         self.assertEqual(b.endTime, "17:00")
-        c = CreateLab.objects.get(courseNumber=54911, sectionNumber="001")
+        c = CreateLab.createLab().get(courseNumber=54911, sectionNumber="001")
         self.assertEqual(c.meetingDays, "M,W")
         self.assertEqual(c.starTime, "10:00")
         self.assertEqual(c.endTime, "12:00")
@@ -40,6 +39,8 @@ class Test_CreateLab(TestCase):
 
     def test_lab_no_sectionNumber(self):
         self.assertEqual(models.command, "createLab 52312 'w' '10:00' '12:00' ")
+
+
 
 
 
