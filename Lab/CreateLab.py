cdfc9d89563851cb6be1b1b31a258b38280aa1d6
Erik <spykim2003@gmail.com>
2019-04-01 21:57:33 -0500

    updated createlab

--- Lab/CreateLab.py
+++ Lab/CreateLab.py
@@ -1,8 +1,16 @@
 from Lab.models import Lab
+from CurrentUserHelper import CurrentUserHelper
 
 
 class CreateLab():
 
     def createLab(self, command):
+        CUH = CurrentUserHelper()
+        if CUH.getCurrentUserTitle() < 3:
+            return "Only administrators and supervisors can create labs"
+        if len(command) > 4 or len(command) < 4:
+            return "createLabs takes 4 arguments: course number,section number, meeting days, start time, and end time"
 
-        pass
+       
+            c.save()
+            return str(c) + " added to database"
