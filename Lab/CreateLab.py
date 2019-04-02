9f4ca0e37a14677e78ee5aa82a2bbe0d614f88c8
Erik <spykim2003@gmail.com>
2019-04-01 22:41:24 -0500

    updated createlab

--- Lab/CreateLab.py
+++ Lab/CreateLab.py
@@ -5,12 +5,13 @@ from CurrentUserHelper import CurrentUserHelper
 class CreateLab():
 
     def createLab(self, command):
-        CUH = CurrentUserHelper()
-        if CUH.getCurrentUserTitle() < 3:
+        cuh = CurrentUserHelper()
+        if cuh.getCurrentUserTitle() < 3:
             return "Permission denied. Only administrators and supervisors can create labs"
         if len(command) > 4 or len(command) < 4:
             return "Please retype the command. " \
-                   "CreateLabs command takes 5 arguments: course number,section number, meeting days, start time, and end time"
+                   "CreateLabs command takes 5 arguments: course number,section number, meeting days, " \
+                   "start time, and end time"
             courseNumber = command[1]
             sectionNumber = command[2]
             meetingDays = command[3]
@@ -22,9 +23,10 @@ class CreateLab():
             elif Lab.objects.get(sectionNumber).exists():
                 raise Exception("Section number already exists")
             else:
-             l = Lab(CourseNumber,Section)
-             l.meetingDays = meetingDays
-            l.classHoursStart = startTime
-            l.classHoursEnd = endTime
-            l.save()
+                l = Lab(CourseNumber, Section)
+                l.meetingDays = meetingDays
+                l.classHoursStart = startTime
+                l.classHoursEnd = endTime
+                l.save()
+
             return str(c) + " added to database"
