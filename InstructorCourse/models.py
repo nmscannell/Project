37621d7d2077fa0e6bc11964e8d8683c55bba229
Erik <spykim2003@gmail.com>
2019-04-02 21:19:21 -0500

    InstructorCourse model updated

--- InstructorCourse/models.py
+++ InstructorCourse/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+
+class InstructorCourse(models.Model):
+    classNumber = models.IntegerField(default=0)
+    userName = models.CharField(max_length=20, default=" ")
+
+    def __str__(self):
+        return None
--- Project/settings.py
+++ Project/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'main',
     'Course',
     'Lab'
+    'InstructorCourse'
 ]
 
 MIDDLEWARE = [
