2fe4f93937ab0ad4235ce3c1797071b7f92ec093
Leuenberger <leuenbe4@uwm.edu>
2019-04-03 12:39:08 -0500

    4.3

--- Lab/models.py
+++ Lab/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 
 class Lab(models.Model):
-    courseNumber = models.IntegerField(default=0)
+    courseNumber = models.IntegerField(default=000)
     sectionNumber = models.CharField(max_length=10, default="000")
     meetingDays = models.CharField(max_length=10, default=" ")
     startTime = models.CharField(max_length=10, default="0000")
