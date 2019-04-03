5b5a199a17a4346b3042f8459e33671a8e9d016b
Leuenberger <leuenbe4@uwm.edu>
2019-04-03 15:50:42 -0500

    got createCourse acceptance tests working

--- Lab/models.py
+++ Lab/models.py
@@ -4,11 +4,12 @@ from django.db import models
 
 
 class Lab(models.Model):
+
     courseNumber = models.IntegerField(default=000)
-    sectionNumber = models.CharField(max_length=10, default="000")
+    sectionNumber = models.IntegerField(max_length=10, default=000)
     meetingDays = models.CharField(max_length=10, default=" ")
-    startTime = models.CharField(max_length=10, default="0000")
-    endTime = models.CharField(max_length=10, default="0000")
+    startTime = models.IntegerField(default=0000)
+    endTime = models.IntegerField(default=0000)
 
     def __str__(self):
         return None
