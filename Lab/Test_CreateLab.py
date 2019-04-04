5fa95ace39875793ebbb844a1eb2ab9cffdbb2c2
Erik <spykim2003@gmail.com>
2019-04-04 00:19:37 -0500

    test_createLab updated

--- Lab/Test_CreateLab.py
+++ Lab/Test_CreateLab.py
@@ -20,31 +20,37 @@ class Test_CreateLab(TestCase):
 
     def test_lab_was_successfully_created(self):
         CreateLab.createLab(self.CL, self.command_create_lab)
-        a = Lab.objects.get(courseNumber=52312, sectionNumber="002")
-        self.assertEqual(a.meetingDays, "W")
-        self.assertEqual(a.starTime, "1000")
-        self.assertEqual(a.endTime, "1200")
-        b = CreateLab.createLab.get(courseNumber=52312, sectionNumber="002")
-        self.assertEqual(b.meetingDays, "F")
-        self.assertEqual(b.starTime, "1400")
-        self.assertEqual(b.endTime, "1700")
-        c = CreateLab.createLab.get(courseNumber=54911, sectionNumber="001")
-        self.assertEqual(c.meetingDays, "M W")
+        a = Lab.objects.get(courseNumber=91231, sectionNumber="001")
+        self.assertEqual(a.courseNumber, 91231)
+        self.assertEqual(a.sectionNumber, "001")
+        self.assertEqual(a.meetingDays, "TR")
+        self.assertEqual(a.starTime, "1300")
+        self.assertEqual(a.endTime, "1500")
+        b = Lab.objects.get(courseNumber=19201, sectionNumber="001")
+        self.assertEqual(b.courseNumber, 19201)
+        self.assertEqual(b.sectionNumber, "001")
+        self.assertEqual(b.meetingDays, "M")
+        self.assertEqual(b.starTime, "1500")
+        self.assertEqual(b.endTime, "1600")
+        c = Lab.objects.get(courseNumber=51999, sectionNumber="004")
+        self.assertEqual(c.courseNumber, 51999)
+        self.assertEqual(c.sectionNumber, "004")
+        self.assertEqual(c.meetingDays, "T")
         self.assertEqual(c.starTime, "1000")
         self.assertEqual(c.endTime, "1200")
 
-    def test_lab_was_already_existed(self):
-            with self.assertRaises(SystemError):
-                self.models.command("createLab 52312 '001' 'w' '1000' '1200' ")
+    def test_section_was_already_existed(self):
+        self.assertEqual((CreateLab.createLab(self.CL, self.command_section_was_already_existed)),
+                         "Course already exists")
 
     def test_lab_no_argument(self):
-        self.assertEqual(models.command, "createLab")
+        self.assertEqual(CreateLab.createLab(self.CL, self.command_create_lab_no_args))
 
     def test_lab_no_courseNumber(self):
-        self.assertEqual(models.command, "createLab '001' 'w' '1000' '1200' ")
+        self.assertEqual(CreateLab.createLab(self.CL, self.command_create_lab_no_courseNumber))
 
     def test_lab_no_sectionNumber(self):
-        self.assertEqual(models.command, "createLab 52312 'w' '1000' '1200' ")
+        self.assertEqual(CreateLab.createLab(self.CL, self.command_create_lab_no_sectionNumber))
 
 
 
