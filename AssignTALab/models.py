9e983b967e7f95a51e11e8fd6c22545b3d11918f
Alex <mblahnik@uwm.edu>
2019-04-04 10:05:51 -0500

    You should be able to run the tests again.

---
+++ AssignTALab/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+from Lab.models import Lab
+from Account.models import Account
+# Create your models here.
+
+
+class assignTALab():
+
+    def assignTALab(self, command):
+        pass
