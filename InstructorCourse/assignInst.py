e5fdd72a8d113eaa086f42a4ba8f8296fd21e3db
Erik <spykim2003@gmail.com>
2019-04-05 02:31:57 -0500

    assignInst updated

--- InstructorCourse/assignInst.py
+++ InstructorCourse/assignInst.py
@@ -1,6 +1,6 @@
-from InstructorCourse.models import InstructorCourse
 from CurrentUserHelper import CurrentUserHelper
 from Account.models import Account
+from InstructorCourse.models import InstructorCourse
 from Course.models import Course
 from InstructorCourse.models import InstructorCourse
 
@@ -14,17 +14,12 @@ class assignInst():
         if len(command) > 3 or len(command) < 3:
             return "Please retype the command. " \
                    "assigninstructorcourse, courseNumber, userName "
-        courseNumber = command[1]
-        userName = command[2]
+        else:
+            course = Course.objects.get(number=command[1])
+            instructor = Account.objects.get(number=command[2])
 
-        try:
-            c = Course.objects.get(number=courseNumber)
-        except Course.DoesNotExist:
-            return "The Course you are trying to assign a course for does not exist"
-
-            a = InstructorCourse.objects.create()
-            a.course = c
-            a.courseNumber = courseNumber
-            a.userName = userName
+            a = InstructorCourse()
+            a.instructor = instructor
+            a.course = course
             a.save()
             return "Assignment successfully completed"
