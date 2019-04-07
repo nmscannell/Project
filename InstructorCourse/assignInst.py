d14cfdcc47bc343a215178eb23a0f900e4fa9f70
Erik <spykim2003@gmail.com>
2019-04-06 19:46:14 -0500

    assignInst debugging

--- InstructorCourse/assignInst.py
+++ InstructorCourse/assignInst.py
@@ -13,6 +13,8 @@ class assignInst():
         #   return "Permission denied. Only supervisors can assign instructor to courses"
         if len(command) < 3:
             return "Please, type the command in the following format assigninstructorcourse classNumber username"
+        if Course.objects.filter(number=command[2]).exists():
+            return "Course already exists"
         if not Course.objects.filter(userName=command[2]).exists():
             return "Please, type the course number"
         if not Account.objects.filter(number=command[1]).exists():
