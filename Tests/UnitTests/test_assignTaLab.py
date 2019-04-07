a928ede6155c2424ac0bf1905e3969e361967f07
mblahnik <mblahnik@uwm.edu>
2019-04-06 20:23:33 -0500

    AssignTALAb works?

--- Tests/UnitTests/test_assignTaLab.py
+++ Tests/UnitTests/test_assignTaLab.py
@@ -12,6 +12,7 @@ class TestAssignTaLab(TestCase):
         Account.objects.create(userName='hsimpson', title='1')
         Course.objects.create(number='361')
         Course.objects.create(number='351')
+        self.TA = Account.objects.get(userName='hsimpson')
         self.Course1 = Course.objects.get(number='361')
         self.Course2 = Course.objects.get(number='351')
         Lab.objects.create(sectionNumber=804, course=self.Course1)
@@ -39,8 +40,27 @@ class TestAssignTaLab(TestCase):
 
         self.assertFalse(TaLab.objects.exists())
 
+    def test_assignTaLab_Lab_full(self):
+        Account.objects.create(userName='taman', title='1')
+        ta = Account.objects.get(userName='taman')
+        Lab.objects.create(sectionNumber=801, course=self.Course2)
+        lab = Lab.objects.get(sectionNumber=801)
+        TaLab.objects.create(TA=ta, Lab=lab)
+
+        self.assertEqual(len(TaLab.objects.filter()), 1)
+
+        self.assertEqual(self.atl.assignTaLab(["assigntalab", "hsimpson", "351", "801"]), "Lab section already assigned")
+
+        self.assertFalse(TaLab.objects.filter(TA=self.TA).exists())
+
+        self.assertEqual(len(TaLab.objects.filter()), 1)
+
     def test_assignTaLab_success(self):
+        self.assertEqual(len(TaLab.objects.filter()), 0)
+
         self.assertEqual(self.atl.assignTaLab(["assigntalab", 'hsimpson', "361", "804"]), "TA successfully assigned")
+
+        self.assertEqual(len(TaLab.objects.filter()), 1)
 
         self.assertTrue(TaLab.objects.exists())
 
--- Tests/UnitTests/test_loginHelper.py
+++ Tests/UnitTests/test_loginHelper.py
@@ -18,22 +18,21 @@ class TestLoginHelper(TestCase):
         #test Successful login
         self.assertEqual(self.login.login(self.Command1), "Logged in as hsimpson")
         A = Account.objects.get(userName="hsimpson")
-        Account.objects.exists()
         self.assertTrue(A.currentUser)
+
+        self.assertTrue(Account.objects.filter(currentUser='True').exists())
 
     def test_login_wrong_password(self):
         # test wrong password
         self.assertEqual(self.login.login(self.Command2), "Incorrect password")
 
-        with self.assertRaises(Account.DoesNotExist):
-            A = Account.objects.get(currentUser='True')
+        self.assertFalse(Account.objects.filter(currentUser='True').exists())
 
     def test_login_account_not_found(self):
         #test logging in with an Account not in the database
         self.assertEqual(self.login.login(self.Command3), "Account Not Found")
 
-        with self.assertRaises(Account.DoesNotExist):
-            A = Account.objects.get(currentUser='True')
+        self.assertFalse(Account.objects.filter(currentUser='True').exists())
 
     def test_login_2_arguments(self):
         #User doesn't enter enough arugments
@@ -41,8 +40,7 @@ class TestLoginHelper(TestCase):
                          "Your command is missing arguments.  Please enter your command in the following format: "
                          "login userName password")
 
-        with self.assertRaises(Account.DoesNotExist):
-            A = Account.objects.get(currentUser='True')
+        self.assertFalse(Account.objects.filter(currentUser='True').exists())
 
     def test_login_4_arguments(self):
         #User enters too many argumants
@@ -50,25 +48,29 @@ class TestLoginHelper(TestCase):
                          "Your command is missing arguments.  Please enter your command in the following format: "
                          "login userName password")
 
-        with self.assertRaises(Account.DoesNotExist):
-            A = Account.objects.get(currentUser='True')
+        self.assertFalse(Account.objects.filter(currentUser='True').exists())
 
     def test_login_2_accounts(self):
         Account.objects.create(userName='Bob', password='wrongPassword', firstName='Bob', currentUser='True')
         #User tries to login when an Account is already logged in
         self.assertEqual(self.login.login(self.Command1), "A user is already logged in")
 
+        self.assertEqual(len(Account.objects.filter(currentUser='True')), 1)
+
     def test_logout_success(self):
         Account.objects.create(userName='Bob', password='wrongPassword', firstName='Bob', currentUser='True')
+
+        self.assertTrue(Account.objects.filter(currentUser='True').exists())
+
         self.assertEqual(self.login.logout(), "Successfully logged out")
 
-        with self.assertRaises(Account.DoesNotExist):
-            A = Account.objects.get(currentUser='True')
+        self.assertFalse(Account.objects.filter(currentUser='True').exists())
 
     def test_logout_not_logged_in(self):
         #loging out when no Account is the current user
 
+        self.assertFalse(Account.objects.filter(currentUser='True').exists())
+
         self.assertEqual(self.login.logout(), "Please log in First")
 
-        with self.assertRaises(Account.DoesNotExist):
-            A = Account.objects.get(currentUser='True')
+        self.assertFalse(Account.objects.filter(currentUser='True').exists())
