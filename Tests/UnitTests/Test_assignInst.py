89490d93736ca28e7bce7c1af95291b2bad67316
Erik <spykim2003@gmail.com>
2019-04-08 00:42:14 -0500

    more unit tests updated for assignInst

--- Tests/UnitTests/Test_assignInst.py
+++ Tests/UnitTests/Test_assignInst.py
@@ -10,19 +10,26 @@ class TestAssignInst(TestCase):
     def setUp(self):
         self.account1 = Account.objects.create(userName="cheng41", title="2")
         self.course1 = Course.objects.create(number="535", name="AlgorithmDesignAndAnalysis")
-        self.course2 = Course.objects.create(number="317")
+        self.account2 = Account.objects.create(userName="suzuki15", title="2")
+        self.course2 = Course.objects.create(number="317", name="DiscreteInformationStructures")
+        self.account3 = Account.objects.create(userName="ImNotInstructor", title="1")
         self.AI = assignInst()
 
     def test_assignInst_successfully_created(self):
 
         self.assertEqual(self.AI.assignInst(["assigninstructorcourse", "cheng41", "535"]),
                          "Instructor was successfully assigned to class")
+        self.assertEqual(self.AI.assignInst(["assigninstructorcourse", "suzuki15", "317"]),
+                         "Instructor was successfully assigned to class")
         self.assertTrue(InstructorCourse.objects.exists())
         A = Account.objects.get(userName="cheng41")
         B = Course.objects.get(number="535")
-
+        C = Account.objects.get(userName="suzuki15")
+        D = Course.objects.get(number="317")
         self.assertEqual(A.userName, "cheng41")
         self.assertEqual(B.number, 535)
+        self.assertEqual(C.userName, "suzuki15")
+        self.assertEqual(D.number, 317)
 
     def test_assignInt_already_exists(self):
         InstructorCourse.objects.create(Instructor=self.account1, Course=self.course1)
