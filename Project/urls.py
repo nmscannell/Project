cc4b44569c631ec156c8cdb489382d6e6f8f6110
mblahnik <mblahnik@uwm.edu>
2019-04-09 18:28:35 -0500

    Login page will work!

--- Project/urls.py
+++ Project/urls.py
@@ -13,14 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
 from django.conf.urls import url
 from django.urls import path
 from django.contrib import admin
-from main import views
+from main import views as main
+from login import views as login
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", views.Home.as_view())
+    path("", main.Home.as_view()),
+    path("login/", login.loginPage.as_view())
 ]
--- login/views.py
+++ login/views.py
@@ -3,28 +3,36 @@ from django.views import View
 from login.models import LoginHelper
 # Create your views here.
 
+
 class loginPage(View):
+
+    def get(self, request):
+        return render(request, 'loginscreen.html')
 
     def post(self, request):
         LH = LoginHelper()
         username = str(request.POST["username"])
         password = str(request.POST["password"])
 
-        command = ["", username, password]
+        commandlist = ["", username, password]
 
         try:
-            check = LH.login(command)
+            check = LH.login(commandlist)
 
             if check == 1:
-                return "TA homepage"
+                LH.logout()
+                return render(request, 'loginscreen.html', {"message": "Ta login"})
             if check == 2:
-                return "Instructor homepage"
+                LH.logout()
+                return render(request, 'loginscreen.html', {"message": "Instructor login"})
             if check == 3:
-                return "Admin homepage"
+                LH.logout()
+                return render(request, 'loginscreen.html', {"message": "Admin login"})
             if check == 4:
-                return "Supervisor homepage"
+                LH.logout()
+                return render(request, 'loginscreen.html', {"message": "Supervisor login"})
 
         except Exception as e:
 
-            return render(request, 'main/index.html', {"message": str(e)})
+            return render(request, 'loginscreen.html', {"message": str(e)})
 
